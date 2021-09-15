c22a9c67027fa1368bf2ea88f66fa6fabf4dd113
Gugo <Gugo@PC-HG01>
2021-09-15 00:09:17 -0300

    V 1.0

---
+++ src/br/edu/fateczl/hugobispo/pilhaint/No.java
@@ -0,0 +1,8 @@
+package br.edu.fateczl.hugobispo.pilhaint;
+
+public class No {
+
+	int dado;
+	No proximo;
+	
+}
---
+++ src/br/edu/fateczl/hugobispo/pilhastring/No.java
@@ -0,0 +1,10 @@
+package br.edu.fateczl.hugobispo.pilhastring;
+
+import br.edu.fateczl.hugobispo.pilhastring.No;
+
+public class No {
+
+	String dado;
+	No proximo;
+	
+}
